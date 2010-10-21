b7e176710806037bb9d24b6570690dee552119bb
Suchart Chokphichitchai <dragon_html@hotmail.com>
2010-10-21 15:56:58 +0700

    first commit

---
+++ thai_php_functions.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ *
+ * @param <string> $s
+ * @return <string> htmlspecialchars short name
+ */
+function h($s){
+	return htmlspecialchars($s,ENT_QUOTES,'UTF-8');
+	//return htmlspecialchars($s,ENT_QUOTES,'ISO-8859-1'); //tis-620,windows-874
+}
+
+/**
+ *
+ * @param <string> $s
+ * @return <int> convert string to int short name
+ */
+function i($s){
+        return intval( $s );
+}
+
+
+
+?>
