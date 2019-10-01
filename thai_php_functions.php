454af26321830c2b8dcbe9b03eb8c6cfcccfbe57
mrjummy <46760971+mrjummy@users.noreply.github.com>
2019-10-01 23:25:35 +0700

    format code

--- thai_php_functions.php
+++ thai_php_functions.php
@@ -6,8 +6,7 @@
  * @return <string> htmlspecialchars short name
  */
 function h($s){
-	return htmlspecialchars($s,ENT_QUOTES,'UTF-8');
-	//return htmlspecialchars($s,ENT_QUOTES,'ISO-8859-1'); //tis-620,windows-874
+    return htmlspecialchars($s,ENT_QUOTES,'UTF-8');
 }
 
 /**
@@ -16,9 +15,6 @@ function h($s){
  * @return <int> convert string to int short name
  */
 function i($s){        
-        return intval($s);
+    return intval($s);
 }
-
-
-
 ?>
